8e5ec9f44c07499d2fc5a3823899f860935b4639
MichaelReid85 <Mike1109r@yahoo.com>
2023-11-13 12:32:53 -0800

    Create ball and movement.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
-
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -8,8 +9,10 @@ screen.setup(800, 600)
 screen.title("Pong - The classic arcade game!")
 screen.tracer(0)
 
+
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 
 screen.listen()
@@ -18,9 +21,12 @@ screen.onkeypress(l_paddle.go_down, "s")
 screen.onkeypress(r_paddle.go_up, "Up")
 screen.onkeypress(r_paddle.go_down, "Down")
 
+
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 
 screen.exitonclick()

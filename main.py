e8a9ea99bf5e92e1aba74ff64053588e4f8cf566
MichaelReid85 <Mike1109r@yahoo.com>
2023-11-13 10:55:23 -0800

    Set up Screen attributes

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-from turtle import Turtle
+import turtle
+from turtle import Turtle, Screen
 
 tim = Turtle()
+
+screen = Screen()
+turtle.title("Pong - The classic arcade game!")
+turtle.bgcolor("black")
+turtle.setup(800, 600)
+
+
+
+
+
+
+
+screen.exitonclick()

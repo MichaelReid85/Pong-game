803e1b538b18a804a143bfd7861230e5cc744147
MichaelReid85 <Mike1109r@yahoo.com>
2023-11-13 11:29:27 -0800

    Created 1st paddle

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import turtle
 from turtle import Turtle, Screen
 
-tim = Turtle()
+paddle = Turtle()
 
 screen = Screen()
-turtle.title("Pong - The classic arcade game!")
-turtle.bgcolor("black")
-turtle.setup(800, 600)
+screen.bgcolor("black")
+screen.setup(800, 600)
+screen.title("Pong - The classic arcade game!")
+
+paddle.penup()
+paddle.color("black")
+paddle.speed("fastest")
+paddle.shape("square")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.setposition(350, 0)
+paddle.color("white")
 
 
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() +-20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
 
 
 
